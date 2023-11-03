ac58dae10d341e4594815d954b8c9ff101cfb161
Raj Prajapati <87031647+rjfantusty@users.noreply.github.com>
2023-11-03 20:58:03 +0530

    fetching list of appointment

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+const key="a3d34d73d5704de28408fbbac06100f3"
+const url=`https://crudcrud.com/api/${key}/appointments`
 function formhandler(e){
     e.preventDefault();
     console.log(e.target.Name.value)
@@ -10,14 +11,46 @@ function formhandler(e){
     const formdata= new FormData(e.target)
     const jsondata={}
     formdata.forEach((value,key)=>jsondata[key]=value)
-    
-    axios.post('https://crudcrud.com/api/81c45959af094ff3abec0b6f54ebc9c0/appointments',jsondata)
-    .then(data=>console.log(data))
-    .catch(err=>console.error(err))
 
-  
+    axios.post(url,jsondata)
+    .then(data=> window.alert('appointment saved'))
+    .catch(err=>console.log(err))
 
 }
+
+listwrapper=document.getElementById('appointments')
+
+function showList(listdata){
+
+    listdata.map((listitem)=>{
+    let li=document.createElement('li')
+    li.className="list-group-item d-flex justify-content-around"
+        li.innerHTML=`
+        <div>${listitem["Name"]}</div>
+        <div>${listitem["Email"]}</div>
+        <div>${listitem["Phone"]}</div>
+        <div>${listitem["Date"]}</div>
+        <div>${listitem["Time"]}</div>
+        `
+    listwrapper.appendChild(li)
+
+    
+    })
+}
+
+function loadlist(){
+    
+    axios.get(url)
+    .then(res=> showList(res.data))
+    .catch(err=>console.log(err))
+
+
+}
+
+
+
+
+window.addEventListener('DOMContentLoaded',loadlist)
 
 
 
