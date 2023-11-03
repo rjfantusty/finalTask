9e8a2db87ec4ac2ac7b9c6e6149bcb9a6fcbfb42
Raj Prajapati <87031647+rjfantusty@users.noreply.github.com>
2023-11-03 22:11:15 +0530

    added delete feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.addEventListener('DOMContentLoaded',loadlist)
 const key="a3d34d73d5704de28408fbbac06100f3"
 const url=`https://crudcrud.com/api/${key}/appointments`
 function formhandler(e){
@@ -14,23 +15,27 @@ function formhandler(e){
 
     axios.post(url,jsondata)
     .then(data=> window.alert('appointment saved'))
+    .then(()=> location.reload())
     .catch(err=>console.log(err))
 
 }
 
+
 listwrapper=document.getElementById('appointments')
 
 function showList(listdata){
-
+    console.log(listdata)
     listdata.map((listitem)=>{
     let li=document.createElement('li')
     li.className="list-group-item d-flex justify-content-around"
+    li.id=`${listitem._id}`
         li.innerHTML=`
         <div>${listitem["Name"]}</div>
         <div>${listitem["Email"]}</div>
         <div>${listitem["Phone"]}</div>
         <div>${listitem["Date"]}</div>
         <div>${listitem["Time"]}</div>
+        <button type="button" class="btn btn-danger delete" >X</button>
         `
     listwrapper.appendChild(li)
 
@@ -38,10 +43,29 @@ function showList(listdata){
     })
 }
 
-function loadlist(){
+function deleteOneList(e){
+    element=e.target
+    parent=element.parentNode
     
+    axios.delete(`${url}/${parent.id}`)
+    .then(data=>console.log('deleted'))
+    .then(()=>location.reload())
+    .catch(err=>console.log(err))
+}
+
+
+
+function loadlist(){
+    console.log('loading')
     axios.get(url)
     .then(res=> showList(res.data))
+    .then(()=>{
+        let deletebuttons=document.getElementsByClassName('delete')
+        deletebuttons= Array.from(deletebuttons)
+        deletebuttons.map((btn)=>{
+            btn.addEventListener('click',deleteOneList)
+        })
+    })
     .catch(err=>console.log(err))
 
 
@@ -50,7 +74,7 @@ function loadlist(){
 
 
 
-window.addEventListener('DOMContentLoaded',loadlist)
+
 
 
 
