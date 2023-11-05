ede75f8a409fc860c81815a4c401d85d6daa60dc
Raj Prajapati <87031647+rjfantusty@users.noreply.github.com>
2023-11-05 21:26:33 +0530

    adding edit appointment feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded',loadlist)
-const key="a3d34d73d5704de28408fbbac06100f3"
+const key="d1f06f7fc3d446fd906148d6954c95be"
 const url=`https://crudcrud.com/api/${key}/appointments`
 function formhandler(e){
     e.preventDefault();
@@ -15,6 +15,27 @@ function formhandler(e){
 
     axios.post(url,jsondata)
     .then(data=> window.alert('appointment saved'))
+    .then(()=> location.reload())
+    .catch(err=>console.log(err))
+
+}
+function edithandler(e){
+    e.preventDefault();
+    console.log(e.element)
+    console.log(e.target.Name.value)
+    console.log(e.target.Email.value)
+    console.log(e.target.Phone.value)
+    console.log(e.target.Date.value)
+    console.log(e.target.Time.value)
+
+    let clientid=document.getElementById('clientid').textContent
+
+    const formdata= new FormData(e.target)
+    const jsondata={}
+    formdata.forEach((value,key)=>jsondata[key]=value)
+
+    axios.put(`${url}/${clientid}`,jsondata)
+    .then(data=> window.alert('appointment edited'))
     .then(()=> location.reload())
     .catch(err=>console.log(err))
 
@@ -35,6 +56,7 @@ function showList(listdata){
         <div>${listitem["Phone"]}</div>
         <div>${listitem["Date"]}</div>
         <div>${listitem["Time"]}</div>
+        <button type="button"  class="btn btn-danger edit" >edit</button>
         <button type="button" class="btn btn-danger delete" >X</button>
         `
     listwrapper.appendChild(li)
@@ -53,6 +75,30 @@ function deleteOneList(e){
     .catch(err=>console.log(err))
 }
 
+function editOneList(e){
+    e.preventDefault()
+    document.getElementById('getform').classList.add('d-none')
+    let editform=document.getElementById('editform').classList.remove('d-none')
+    
+    element=e.target
+    parent=element.parentNode
+    
+    axios.get(`${url}/${parent.id}`)
+    .then((res)=>{
+        let userdata=res.data
+       let {_id,Name,Email,Phone,Date,Time}=userdata
+       
+       document.getElementById('clientid').innerText=_id 
+       document.getElementById('editname').value=Name 
+       document.getElementById('editemail').value=Email
+       document.getElementById('editphone').value=Phone
+       document.getElementById('editdate').value=Date
+       document.getElementById('edittime').value=Time
+
+    })
+    .catch(err=>console.log(err))
+}
+
 
 
 function loadlist(){
@@ -64,6 +110,12 @@ function loadlist(){
         deletebuttons= Array.from(deletebuttons)
         deletebuttons.map((btn)=>{
             btn.addEventListener('click',deleteOneList)
+        })
+
+        let editbuttons=document.getElementsByClassName('edit')
+        editbuttons= Array.from(editbuttons)
+        editbuttons.map((btn)=>{
+            btn.addEventListener('click',editOneList)
         })
     })
     .catch(err=>console.log(err))
